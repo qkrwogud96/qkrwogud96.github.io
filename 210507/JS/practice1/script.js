b8f2f682422bc5d3454a591aaade44ece89ab693
qkrwogud96 <qkrwogud96@naver.com>
2021-07-01 17:54:14 +0900

    up

--- 210507/JS/practice1/script.js
+++ 210507/JS/practice1/script.js
@@ -3,23 +3,39 @@ const imgContents = document.querySelectorAll(".img_contents");
 const WIDTH = 300; // 이미지 크기
 const SLIDESPEED = 300; //슬라이드 속도
 const IMAGE_LEN = imgContents.length; //슬라이드 이미지 길이
-let imgCurIndex = 0; //슬라이드 이미지 위치 
+let imgCurIndex = 0; //슬라이드 이미지 위치
+console.log(imgContents);
 
+const cloneobj = () => {
+  const firstChildNode = imgWrap.firstElementChild;
+  const lastChildNode = imgWrap.lastElementChild;
+  const cloneFirst = firstChildNode.cloneNode(true);
+  const cloneLast = lastChildNode.cloneNode(true);
+  imgWrap.appendChild(cloneFirst);
+  imgWrap.insertBefore(cloneLast, firstChildNode);
+};
 
 //Next 버튼 이벤트
 function clickEventNext() {
-  console.log("click btn next");
   imgCurIndex++;
-  if(imgCurIndex >= 5){
-      imgCurIndex = 0;
+  if (imgCurIndex >= IMAGE_LEN) {
+    imgCurIndex = 0;
+    setTimeout(function () {
+      // imgWrap.style.transition = 0;
+      // imgWrap.style.transform = `translateX(-${WIDTH * imgCurIndex}px)`;
+    }, SLIDESPEED);
   }
-  imgWrap.style.transition = SLIDESPEED + 'ms';
-  imgWrap.style.transform = `translateX(-${WIDTH * imgCurIndex}px)`; 
-  console.log( imgWrap.style.transform );
+  imgWrap.style.transition = SLIDESPEED + "ms";
+  imgWrap.style.transform = `translateX(-${WIDTH * imgCurIndex}px)`;
 }
 //Prev 버튼 이벤트
 function clickEventPrev() {
-  console.log("click btn prev");
+  imgCurIndex--;
+  if (imgCurIndex <= 0) {
+    imgCurIndex = IMAGE_LEN;
+  }
+  imgWrap.style.transition = SLIDESPEED + "ms";
+  imgWrap.style.transform = `translateX(-${WIDTH * (imgCurIndex - 1)}px)`;
 }
 //버튼 클릭 처리 함수선언
 const clickEventBtn = () => {
@@ -30,7 +46,7 @@ const clickEventBtn = () => {
 };
 const init = () => {
   clickEventBtn();
-
-  imgWrap.style.width = WIDTH * IMAGE_LEN + "px";
+  cloneobj();
+  imgWrap.style.width = WIDTH * (IMAGE_LEN+2) + "px";
 };
 init();
