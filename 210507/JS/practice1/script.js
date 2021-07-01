52e1726b234ba10e379ad0c11007d7a3fbfad452
qkrwogud96 <83631761+qkrwogud96@users.noreply.github.com>
2021-07-02 03:17:23 +0900

    up

--- 210507/JS/practice1/script.js
+++ 210507/JS/practice1/script.js
@@ -4,7 +4,7 @@ const WIDTH = 300; // 이미지 크기
 const SLIDESPEED = 300; //슬라이드 속도
 const IMAGE_LEN = imgContents.length; //슬라이드 이미지 길이
 let imgCurIndex = 0; //슬라이드 이미지 위치
-console.log(imgContents);
+console.log(IMAGE_LEN);
 
 const cloneobj = () => {
   const firstChildNode = imgWrap.firstElementChild;
@@ -18,25 +18,39 @@ const cloneobj = () => {
 //Next 버튼 이벤트
 function clickEventNext() {
   imgCurIndex++;
-  if (imgCurIndex >= IMAGE_LEN) {
-    imgCurIndex = 0;
-    setTimeout(function () {
-      // imgWrap.style.transition = 0;
-      // imgWrap.style.transform = `translateX(-${WIDTH * imgCurIndex}px)`;
-    }, SLIDESPEED);
+  if (imgCurIndex <= IMAGE_LEN + 1) {
+    console.log(imgCurIndex);
+
+    imgWrap.style.transition = SLIDESPEED + "ms";
+    imgWrap.style.transform = `translateX(-${WIDTH * (imgCurIndex + 1)}px)`;
   }
-  imgWrap.style.transition = SLIDESPEED + "ms";
-  imgWrap.style.transform = `translateX(-${WIDTH * imgCurIndex}px)`;
+  if (imgCurIndex === IMAGE_LEN) {
+    setTimeout(function () {
+      imgWrap.style.transition = 0 + "ms";
+      imgWrap.style.transform = `translateX(-${WIDTH}px)`;
+    }, SLIDESPEED);
+    imgCurIndex = 0;
+    console.log(imgCurIndex);
+  }
 }
 //Prev 버튼 이벤트
 function clickEventPrev() {
   imgCurIndex--;
-  if (imgCurIndex <= 0) {
-    imgCurIndex = IMAGE_LEN;
+  if (imgCurIndex >= -1) {
+    console.log(imgCurIndex);
+    imgWrap.style.transition = SLIDESPEED + "ms";
+    imgWrap.style.transform = `translateX(-${WIDTH * (imgCurIndex + 1)}px)`;
   }
-  imgWrap.style.transition = SLIDESPEED + "ms";
-  imgWrap.style.transform = `translateX(-${WIDTH * (imgCurIndex - 1)}px)`;
+  if (imgCurIndex === -1) {
+    console.log(imgCurIndex);
+    setTimeout(function () {
+      imgWrap.style.transition = 0 + "ms";
+      imgWrap.style.transform = `translateX(-${WIDTH * IMAGE_LEN}px)`;
+    }, SLIDESPEED);
+    imgCurIndex = IMAGE_LEN-1;
+  }
 }
+
 //버튼 클릭 처리 함수선언
 const clickEventBtn = () => {
   const btnPrev = document.querySelector(".btn_prev");
@@ -47,6 +61,7 @@ const clickEventBtn = () => {
 const init = () => {
   clickEventBtn();
   cloneobj();
-  imgWrap.style.width = WIDTH * (IMAGE_LEN+2) + "px";
+  imgWrap.style.width = WIDTH * (IMAGE_LEN + 2) + "px";
+  imgWrap.style.transform = `translateX(-${WIDTH}px)`;
 };
 init();
