f8f395444de75b3ab4f967dcf21f74afd749c6b9
qkrwogud96 <qkrwogud96@naver.com>
2021-07-15 18:36:12 +0900

    up

--- 210507/JS/3dscroll/scrollExam.js
+++ 210507/JS/3dscroll/scrollExam.js
@@ -1,5 +1,24 @@
 const processBar = document.querySelector(".process-bar");
 const boxObj = document.querySelector(".box");
+const stageObj = document.querySelector(".stage");
+const menuObj = document.querySelector('#gnb');
+const zMoveFocus = [-500, -300, 20, 300, 500];
+
+const getViewIndex = (curPos) => {
+  for (let i = 0; i < zMoveFocus.length; i++) {
+    if (curPos >= zMoveFocus[i] && curPos < zMoveFocus[i + 1]) {
+      return i;
+    }
+  }
+};
+const menuActive = (idx) =>{
+  const activeObj = document.querySelector('.active');
+  activeObj.classList.remove('active');
+  menuObj.children[idx].classList.add('active');
+}
+const resizeEvent = () => {
+  contentScroll = document.documentElement.scrollHeight - window.innerHeight;
+};
 const scrollEvent = () => {
   const bodyHeight = document.documentElement.scrollHeight;
   const scrollHeight = window.innerHeight;
@@ -10,17 +29,32 @@ const scrollEvent = () => {
 
   const boxPercent = -500 + (curScroll / (bodyHeight - scrollHeight)) * 995;
   boxObj.style.transform = `translateZ(${boxPercent}vw)`;
+
+  const index = getViewIndex(boxPercent);
+  menuActive(index);
+
 };
 const moveEvent = (e) => {
   //   console.log(e.clientX-(window.innerWidth * 0.5), e.clientY- (window.innerHeight * 0.5));
   const pointX = -1 + (e.clientX / window.innerWidth) * 2;
   const pointY = 1 - (e.clientY / window.innerHeight) * 2;
-     console.log(pointX,pointY);
 
-  boxObj.style.transform = `rotateX(${3 * pointY}deg) rotateY(${3 * pointX}deg)`;
+  stageObj.style.transform = `rotateX(${5 * pointY}deg) rotateY(${5 * pointX}deg)`;
 };
+const menuClickEvent = (e) =>{
+  e.preventDefault();
+  const target = e.target;
+  if( target.tagName === 'A'){
+    const idx = target.getAttribute('href');
+    boxObj.style.transform = `translateZ(${zMoveFocus[idx]}vw)`;
+    menuActive(idx);
+  }
+}
 const init = () => {
-  window.addEventListener("mousemove", moveEvent);
+  resizeEvent();
+  window.addEventListener("resize", resizeEvent);
   window.addEventListener("scroll", scrollEvent);
+  window.addEventListener("mousemove", moveEvent);
+  menuObj.addEventListener('click', menuClickEvent);
 };
 init();
--- 210507/JS/self_interactive/interactive.js
+++ 210507/JS/self_interactive/interactive.js
@@ -1,10 +1,12 @@
 
+
+
 const scrollEvent = () =>{
     const totalHeight = document.documentElement.scrollHeight-window.innerHeight;    
-    const percent = pageYOffset / totalHeight * 1000; //스크롤 남은 퍼센테이지
+    const percent = pageYOffset / totalHeight * 900; //스크롤 남은 퍼센테이지
 
-    const zBoxObj = document.querySelector('#zbox');
-    zBoxObj.style.transform = `translateZ(${-500 + percent}px)`
+    const zBoxObj = document.querySelector('#box');
+    zBoxObj.style.transform = `translateZ(${-500 + percent}vh)`
 }
 const init = () =>{
     window.addEventListener('scroll', scrollEvent);
