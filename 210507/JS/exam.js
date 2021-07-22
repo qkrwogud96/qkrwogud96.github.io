56b4dc16f78907a1a1405bef73d64971fcecb5d8
qkrwogud96 <qkrwogud96@naver.com>
2021-07-22 16:58:51 +0900

    up

---
+++ 210507/JS/exam.js
@@ -0,0 +1,30 @@
+// const num = Math.ceil(Math.random()*3);  ceil == 반올림 // random 0~1수.
+
+// let inputData = prompt('전화번호를 입력하세요.');
+// let change = inputData.toUpperCase();
+// document.write( '대문자변환 : ' + change );
+
+// let userNum = prompt('전화번호를 입력하세요');
+// let result = userNum.substring(0, userNum.length -4) + '****'; // a,b   == a~ b까지만 출력
+// alert( result );
+
+// const arrUrl = ["co.kr", "com", "net", "or.kr", "go.kr"];
+// let check1 = false;
+// let check2 = false;
+
+// let userUrl = prompt("이메일 주소를 입력하세요.");
+
+// if( userUrl.indexOf('@' > 0)){ //입력받은 값이 있나 없나 확인.
+//     check1 = true;
+// }
+// for(let obj of arrUrl){
+//     if( userUrl.indexOf(obj) > 0){
+//         check2 = true;
+//     }
+// }
+// if( check1 && check2){
+//     alert( userUrl );
+// }
+// else{
+//     alert( '잘못 입력하셨습니다.');
+// }
