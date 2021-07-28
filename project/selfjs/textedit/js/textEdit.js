55e3ac6ce0301a7cd112d6fd480dc1e92a615cde
qkrwogud96 <83631761+qkrwogud96@users.noreply.github.com>
2021-07-29 01:46:48 +0900

    up

--- project/selfjs/textedit/js/textEdit.js
+++ project/selfjs/textedit/js/textEdit.js
@@ -1,24 +1,25 @@
 const buttons = document.querySelectorAll("button");
-const editText = document.querySelector(".text_edit span");
-const fontSize = document.querySelector("p");
 const inputs = document.querySelectorAll("input");
 const colors = document.querySelectorAll('.check_color div');
+const editText = document.querySelector(".text_edit span");
+const fontSize = document.querySelector("p");
 
-let curSize = 16;
-let curValue = "color";
-let curColor = "white";
+let curSize = 16; // 폰트 사이즈
+let curValue = "color"; // 현재 선택된 색상
+let curColor = "white"; // 현재 선택된 값
+
 function fontValue() {
   fontSize.innerText = `${curSize}px`;
   editText.style.fontSize = `${curSize}px`;
-}
+} // 폰트 크기 변경 및 출력
 function fontStyle(obj) {
   editText.classList.toggle(obj);
-}
+}  // 폰트 스타일 변경
 function active(obj){
    const act = document.querySelector('.active');
    act.classList.toggle('active');
    obj.classList.toggle('active');
-}
+} //클릭 액티브 처리 함수
 function change(){
    if(curValue == 'color'){
         editText.style.color = curColor;
@@ -26,7 +27,7 @@ function change(){
    else{
         editText.style.backgroundColor = curColor;
    }
-}
+} // 변경( 색상 , 값 ) 처리함수
 const targetEvent = (obj) => {
     switch (obj) {
       case "dec":
@@ -69,7 +70,7 @@ const inputClickEvent = (e) => {
       curValue = "color";
     }
   };
-const colorsClickEvent = (e) =>{
+const colorClickEvent = (e) =>{
     const target = e.target;
     curColor = target.getAttribute('color');
     active(target);
@@ -82,7 +83,7 @@ const init = () => {
     obj.addEventListener("change", inputClickEvent);
   });
   colors.forEach((obj) => {
-    obj.addEventListener("click", colorsClickEvent);
+    obj.addEventListener("click", colorClickEvent);
   });
 };
 init();
