d096977a8e4a0ef999a481ab6381e1ea62b904a2
qkrwogud96 <qkrwogud96@naver.com>
2021-08-13 16:52:21 +0900

    up

--- project/WebDesign/javascript.js
+++ project/WebDesign/javascript.js
@@ -1,21 +1,34 @@
-$(function(){
-    $('header > ul > li').on('mouseover', function(){
-        $(this).css('background-color', 'pink');
-        $(this).find('.submenu').stop().slideDown();
-    });
-    $('header > ul > li').on('mouseout', function(){
-        $(this).find('.submenu').stop().slideUp();
-    });
-    let curPos = 0;
-    setInterval(function(){
-        if(curPos < 2){
-            curPos++;
-        }
-        else{
-            curPos = 0;
-        }
-        $('.slideImg').eq(curPos).siblings().fadeOut();
-        $('.slideImg').eq(curPos).fadeIn();
+$(function () {
+  $("header > ul > li").on("mouseover", function () {
+    $(this).find(".submenu").stop().slideDown();
+  });
+  $("header > ul > li").on("mouseout", function () {
+    $(this).find(".submenu").stop().slideUp();
+  });
+  let curPos = 0;
+  setInterval(function () {
+    if (curPos < 2) {
+      curPos++;
+    } else {
+      curPos = 0;
+    }
+    $(".slideImg").eq(curPos).siblings().fadeOut();
+    $(".slideImg").eq(curPos).fadeIn();
+  }, 3000);
 
-    },3000);
+  $("#img-section > content > .menu").on("click", function () {
+    $(".active").removeClass("active");
+    $(this).addClass("active");
+    $(".on").removeClass("on");
+    $(this).next(".contents").addClass("on");
+  });
+
+  $("#img-section > content > .contents1 > div")
+    .eq(0)
+    .on("click", function () {
+      $(".pop-up").fadeIn();
+    });
+  $(".modal > .exit").on("click", function () {
+    $(".pop-up").fadeOut();
+  });
 });
