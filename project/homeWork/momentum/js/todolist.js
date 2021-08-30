7b27f1d1234835cff1fb8ba38d6193759cdf1517
qkrwogud96 <qkrwogud96@naver.com>
2021-08-30 11:06:54 +0900

    up

--- project/homeWork/momentum/js/todolist.js
+++ project/homeWork/momentum/js/todolist.js
@@ -3,49 +3,57 @@ const todoInputElem = todoFormElem.querySelector("input");
 const todoUl = document.querySelector("#to-do-ul");
 
 let curList = 0;
-
 const deleteBtnEvent = (e) => {
   const target = e.target.parentElement;
+  console.log(target.lastChild.innerText);
+  for (let i = 1; i <= localStorage.length - 1; i++) {
+    const keyValue = localStorage.getItem(i);
+    if (keyValue == target.lastChild.innerText) {
+      localStorage.removeItem(i);
+    }
+  }
   target.remove();
   curList--;
 };
-const saveLocal = () =>{
+const saveLocal = () => {
   const lastList = todoUl.lastChild;
   const Text = lastList.lastChild.innerText;
-  window.localStorage.setItem(curList+1, Text);
+  window.localStorage.setItem(curList + 1, Text);
   curList++;
-}
+};
 const addTodoList = (value) => {
   const li = document.createElement("li");
   const btn = document.createElement("button");
   const span = document.createElement("span");
 
-  if(curList > 9){
+  if (curList > 9) {
     curList = 9;
     todoUl.lastChild.remove();
     localStorage.removeItem(10);
   }
-  todoInputElem.value = '';
+  todoInputElem.value = "";
   span.innerText = value;
   btn.innerText = "❌";
   li.appendChild(btn);
   li.appendChild(span);
   todoUl.appendChild(li);
   saveLocal();
-  btn.addEventListener('click', deleteBtnEvent);
+  btn.addEventListener("click", deleteBtnEvent);
 };
 const todoSubmitEvent = (event) => {
   event.preventDefault();
   const value = todoInputElem.value;
   addTodoList(value);
 };
-const checkey = () =>{
-  if(localStorage.length){
-    for(let i = 1; i <= localStorage.length; i++){
-        const checkListValue = localStorage.getItem(i);
-        addTodoList(checkListValue);
+const checkey = () => {
+  if (localStorage.length > 1) {
+    for (let i = 1; i <= localStorage.length-1; i++) {
+      if(localStorage.getItem('username')){
+        const ListValue = localStorage.getItem(i);
+        addTodoList(ListValue);
+      }
     }
   }
-}
-window.addEventListener('load', checkey);
+};
+window.addEventListener("load", checkey);
 todoFormElem.addEventListener("submit", todoSubmitEvent);
